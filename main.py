ef6f5cd5d79cd5d474d96f25a968243d1a0d2d2f
CsharpProgramming <110731912+CsharpProgramming@users.noreply.github.com>
2022-08-06 16:52:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+ComputerName = "Steve the Computer"
+UserName = "Admin"
+
+
+def UserInput():
+    UserMsg = input(UserName + ": ")
+    
+    if UserMsg == "hello":
+        print(ComputerName + ": Well, hello " + UserName + "!")
+        UserInput()
+
+    if UserMsg == "goodbye":
+        print(ComputerName + ": Goodbye " + UserName + "!")
+        quit()
+
+    else:
+        print(ComputerName + ": Sorry, may you say that again?")
+        if UserMsg == "no":
+            print(ComputerName + ": addios :(")
+            quit()
+        UserInput()
+
+
+def Main():
+    print(ComputerName + ": Hello boss! Welcome to your chatbot")
+    print(ComputerName + ": My name is " + ComputerName + ", let's start a conversation")
+    UserInput()
+
+Main()
